46610f3122020f9d5ab8aa12286b083b96a24361
kaifei xu <129722907+kaffe666@users.noreply.github.com>
2026-01-09 05:22:15 +0800

    Delete bbp-road-app directory

--- bbp-road-app/frontend/node_modules/.vite/deps/leaflet.js
+++
@@ -1,3 +0,0 @@
-import { t as require_leaflet_src } from "./leaflet-src-DeBApHQz.js";
-
-export default require_leaflet_src();
--- bbp-road-app/frontend/node_modules/.vite/deps/react.js
+++
@@ -1,3 +0,0 @@
-import { t as require_react } from "./react-D34L9Ixo.js";
-
-export default require_react();
